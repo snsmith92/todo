06f9beb1391ca3eba5637351383be914c402471a
snsmith92 <sherzelsmith@gmail.com>
2018-01-03 11:05:42 -0500

    Modified spec tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
   def index
-    render json: Task.all
+    render json: Task.all #instead of HTML, load/show JSON representation of tasks in the database. Thus it does not look into the views folder.
   end 
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe TasksController, type: :controller do
       get :index
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
-      expect(response_value).to eq(2)
+      expect(response_value.count).to eq(2)
     end 
   end 
 end
