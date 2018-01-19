783028aac7baa39b93613544b7fb25b33879029b
snsmith92 <sherzelsmith@gmail.com>
2018-01-19 15:12:24 +0100

    Added support to create new task through RESTful JSON API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,9 +10,14 @@ class TasksController < ApplicationController
     render json: task
   end 
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end 
+
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end 
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end 
   end 
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created " do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end 
+  end 
+
 end
